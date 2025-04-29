495faee10531a27e260902947b485c3997b430b3
Rue87 <mrunya87@gmail.com>
2025-04-29 22:46:40 +0200

    Feat: Initialize task management script and HTML elements

---
+++ script.js
@@ -0,0 +1,4 @@
+//Get task containers from HTML
+const todoContainer = document.getElementById("todo-tasks");
+const doingContainer = document.getElementById("doing-tasks");
+const doneContainer = document.getElementById("done-tasks");
