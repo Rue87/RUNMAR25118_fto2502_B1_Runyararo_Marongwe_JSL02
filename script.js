5e2a3e319b354e0e28d641cc38aeabbebffa7f75
Rue87 <mrunya87@gmail.com>
2025-05-02 18:16:39 +0200

    Style: Format code with Prettier

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-
 function collectTaskData() {
-  
   // This variable is initialized to false and will be set to true if any task is marked as done
   let hasCompletedTask = false;
-  
+
   // Variables to store task data for two tasks
   let task1Title, task1Description, task1Status;
   let task2Title, task2Description, task2Status;
@@ -13,27 +11,36 @@ function collectTaskData() {
     let taskNumber = 0 + i;
     console.log(`collectTaskData(${taskNumber}) called`);
 
-  // Prompt the user for task details(collect iput from user)
+    // Prompt the user for task details(collect iput from user)
     const taskTitle = prompt(`Enter title for task ${taskNumber}:`);
     const taskDescription = prompt(`Enter description for task ${taskNumber}:`);
     let taskStatus;
 
-  // loop to get a valid status for the task (todo, doing, done)
+    // loop to get a valid status for the task (todo, doing, done)
     do {
       // Ask for task status input
-      taskStatus = prompt(`Enter status for task ${taskNumber} (todo, doing, done):`);
+      taskStatus = prompt(
+        `Enter status for task ${taskNumber} (todo, doing, done):`
+      );
 
       if (taskStatus) {
         taskStatus = taskStatus.toLowerCase();
       }
-     
+
       if (
-        taskStatus !== "todo" && taskStatus !== "doing" && taskStatus !== "done") {
+        taskStatus !== "todo" &&
+        taskStatus !== "doing" &&
+        taskStatus !== "done"
+      ) {
         alert("Invalid status. Please enter 'todo', 'doing', or 'done'.");
-        }
-        //The loop will continue until a valid status is entered
-    } while (taskStatus !== "todo" && taskStatus !== "doing" && taskStatus !== "done" );
-   
+      }
+      //The loop will continue until a valid status is entered
+    } while (
+      taskStatus !== "todo" &&
+      taskStatus !== "doing" &&
+      taskStatus !== "done"
+    );
+
     if (taskNumber === 1) {
       task1Title = taskTitle;
       task1Description = taskDescription;
@@ -43,14 +50,14 @@ function collectTaskData() {
       task2Description = taskDescription;
       task2Status = taskStatus;
     }
-   
+
     if (taskStatus === "done") {
       console.log(`Title: ${taskTitle}, status: ${taskStatus}`);
       hasCompletedTask = true;
     }
   }
-  
-    if (!hasCompletedTask) {
-      console.log("No tasks completed, let's go to work!");
-    }
+
+  if (!hasCompletedTask) {
+    console.log("No tasks completed, let's go to work!");
   }
+}
